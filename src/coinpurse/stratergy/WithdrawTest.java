f56c9176fb1efd874f1c75a5be05ac3cdfae1963
kwanwantku <kwanwan.t@ku.th>
2018-03-26 22:28:40 +0700

    forgot to put withdraw test finish

--- src/coinpurse/stratergy/WithdrawTest.java
+++ src/coinpurse/stratergy/WithdrawTest.java
@@ -1,9 +1,6 @@
 package coinpurse.stratergy;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,24 +9,33 @@ import org.junit.*;
 
 import coinpurse.BankNote;
 import coinpurse.Coin;
-import coinpurse.Purse;
+import coinpurse.Money;
 import coinpurse.Valuable;
-
+/**
+ * The withdraw test for testing strategy withdraw.
+ * @author Kwanwan Tantichartkul
+ * @version 1.0
+ * @since 2018.03.25
+ */
 public class WithdrawTest {
 	
-	// a "test fixture" - object to test
-	private Purse purse;
-	// tolerance for comparison
 	private static final double TOL = 1.0E-6;
 	private WithdrawStrategy stratergy;
 	private List<Valuable> listmoney = new ArrayList<Valuable>();
 	private List<Valuable> listmoney2 = new ArrayList<Valuable>();
 	
+	/**
+	 * Set up the strategy.
+	 */
 	@Before
 	public void setUp() {
 		stratergy = new GreedyWithdraw();
+		//stratergy = new RecursiveWithdraw();
 	}
     
+	/**
+	 * Test the normal withdraw
+	 */
     @Test
     public void testNormalWithdraw() {
 		Valuable coin1 = new Coin(5, "Baht");
@@ -46,14 +52,15 @@ public class WithdrawTest {
 		listmoney2.add(coin2);
 		listmoney2.add(coin3);
 		listmoney2.add(banknote1);
-		listmoney2.add(banknote2);
-		stratergy.withdraw(coin1, listmoney);
-		stratergy.withdraw(coin1, listmoney2);
-		assertEquals(listmoney,listmoney2);
+		listmoney2.add(banknote2);	
+		assertEquals(stratergy.withdraw(coin1, listmoney),stratergy.withdraw(coin1, listmoney2));
     }
     
+    /**
+     * Test the impossible withdraw
+     */
     @Test
-    public void textImpossibleWithdraw() {
+    public void testImpossibleWithdraw() {
 		Valuable coin1 = new Coin(10, "Baht");
 		assertNull(stratergy.withdraw(coin1, listmoney));
 		Valuable banknote2 = new BankNote(1, "USD");
@@ -62,12 +69,90 @@ public class WithdrawTest {
 		assertNull(stratergy.withdraw(new BankNote(10, "Ringgit"), listmoney2));
     }
     
+    /**
+     * Test the withdraw another money that not in the list.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testWithdrawAnotherMoneyThatNotInTheList() {
+    		BankNote banknote = new BankNote(10000, "Yen");
+    		BankNote banknote2 = new BankNote(10000, "Yen");
+    		listmoney.add(banknote);
+    		listmoney.add(banknote2);
+    		listmoney.add(null);
+    		listmoney = stratergy.withdraw(new Coin(10, "Baht"), listmoney);
+    }
+    
+    /**
+     * Test the compare money.
+     */
+    @Test(timeout=1000)
+    public void testWithdrawCompareMoney() {
+    		BankNote banknote = new BankNote(10000, "Yen");
+    		listmoney.add(banknote);
+    		listmoney2.add(banknote);
+    		assertEquals(stratergy.withdraw(banknote, listmoney), listmoney2);
+    		listmoney2.remove(banknote);
+    		BankNote banknote2 = new BankNote(5000, "Yen");
+    		listmoney.add(banknote2);
+    		listmoney2.add(banknote2);
+    		assertEquals(stratergy.withdraw(banknote2, listmoney), listmoney2);
+    		listmoney2.remove(banknote2);
+    }
+    
+    /**
+     * Test the multi withdraw money.
+     */
+    @Test(timeout=2000)
+    public void testMultiWithdraw() {
+		Valuable coin1 = new Coin(5, "Baht");
+		Valuable coin2 = new Coin(10, "Baht");
+		Valuable coin3 = new Coin(1, "Baht");
+		Valuable coin4 = new Coin(5, "Baht");
+		listmoney.add(coin1);
+		listmoney.add(coin2);
+		listmoney.add(coin3);
+		listmoney.add(coin4);
+		List<Valuable> testing = stratergy.withdraw(new Coin(21, "Baht"), listmoney);
+		assertEquals(21,sumList(testing),TOL);
+		
+		listmoney.clear();
+		Valuable coin5 = new Coin(5, "Baht");
+		Valuable coin6 = new Coin(10, "Baht");
+		Valuable banknote3 = new BankNote(100, "Baht");
+		Valuable banknote4 = new BankNote(500, "Baht");
+		Valuable banknote5 = new BankNote(1000, "Baht");
+		listmoney.add(coin5);
+		listmoney.add(coin6);
+		listmoney.add(banknote3);
+		listmoney.add(banknote4);
+		listmoney.add(banknote5);
+		testing = stratergy.withdraw(new Money(1615, "Baht"), listmoney);
+		assertEquals(1615,sumList(testing),TOL);
+		testing = stratergy.withdraw(new Money(20, "Baht"), listmoney);
+		assertNull(testing);
+    }
+    
+    /**
+     * Test the impossible withdraw.
+     */
+    @Test
+    public void testWithdrawEmptyList() {
+    		assertNull(stratergy.withdraw(new Coin(10, "Ringgit"), listmoney));
+    }
+    
+    /**
+     * The sum method for sum list of money
+     * @param money is the list of money.
+     * @return the sum solution.
+     */
+    private double sumList(List<Valuable> money) {
+    		double sum = 0.0;
+    		for(Valuable v: money) {
+    			sum += v.getValue();
+    		}
+    		return sum;
+    }
     
     
-    
-    
-    
-    
-	
-	
+
 }
